85826d826a6a9e52016dae0126b12578149ba7cd
icmauro <italo_mauro2000@yahoo.com.br>
2024-06-21 00:10:16 -0300

    entregando continuacao

--- Autenticacao.Autorizacao/Service/TokenService.cs
+++ Autenticacao.Autorizacao/Service/TokenService.cs
@@ -2,6 +2,7 @@
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Autenticacao.Autorizacao.Service
@@ -30,5 +31,73 @@ namespace Autenticacao.Autorizacao.Service
 
             return tokenHandler.WriteToken(token);
         }
+
+        public static string GenerateToken(IEnumerable<Claim> claims)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+
+                Expires = DateTime.Now.AddHours(4),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        public static string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+
+            rng.GetBytes(randomNumber);
+
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        public static ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Settings.Secret)),
+                ValidateLifetime = false
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+
+            if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+                !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Invalid Token");
+
+            return principal;
+        }
+
+        private static List<(string,string)> _refreshToken = new();
+
+        public static void SaveRefreshToken(string nome, string refresToken)
+        {
+            _refreshToken.Add(new(nome, refresToken));
+        }
+
+        public static string GetRefreshToken(string nome)
+        {
+            return _refreshToken.FirstOrDefault(x => x.Item1 == nome).Item2;
+        }
+
+        public static void DeleteRefreshToken(string nome, string refresToken)
+        {
+            var item = _refreshToken.FirstOrDefault(x => x.Item1 == nome && x.Item2 == refresToken);
+            _refreshToken.Remove(item);
+        }
+
     }
 }
